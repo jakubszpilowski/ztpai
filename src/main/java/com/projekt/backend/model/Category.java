2091f42bc239e69cae1985bba070531cd72fd297
jakubszpilowski <jakub.szpilowski@student.pk.edu.pl>
2023-04-27 13:51:44 +0200

    ORM version 1

--- src/main/java/com/projekt/backend/model/Category.java
+++ src/main/java/com/projekt/backend/model/Category.java
@@ -1,2 +1,28 @@
-package com.projekt.backend.model;public class Category {
+package com.projekt.backend.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_category")
+    private long id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_parent")
+    private Category parentCategory;
+
+    @OneToMany(
+                fetch = FetchType.EAGER,
+                mappedBy = "parentCategory",
+                cascade = CascadeType.ALL,
+                orphanRemoval = true
+    )
+    private List<Category> subcategories;
 }
